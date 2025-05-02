3637e432a63b50fbfe48f7d17ce693d4665de2d6
nakayama hiroyuki <yuki@nakayamanoMacBook-Pro.local>
2025-05-02 18:34:50 +0900

    0

--- 0015-readlines/src/main.rs
+++ 0015-readlines/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_to_string;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "example.txt";
+    let str = read_to_string(path)?;
+    println!("{}", str);
+
+    let path = "num.txt";
+    // 改行文字もあるよね
+    let num:i32 = read_to_string(path)?.trim().parse()?;
+    println!("{}", num);
+
+    Ok(())
 }
