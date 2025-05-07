51dadd61f9fb55ea9c331817e870c486871a7023
nakayama hiroyuki <yuki@nakayamanoMacBook-Pro.local>
2025-05-07 11:14:38 +0900

    0016 binary-file read

--- 0016-binary-file/src/main.rs
+++ 0016-binary-file/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::fs::File;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = "test.png";
+    let mut f = File::open(path)?;
+    let mut buf = [0; 10];
+    let n = f.read(&mut buf)?;
+    println!("{:?}",  &buf[..n]);
+    let n = f.read(&mut buf)?;
+    println!("{:?}",  &buf[..n]);
+
+    // Read the rest of the file into a vector
+    let mut buf_vec = Vec::new();
+    let _ = f.read_to_end(buf_vec.as_mut())?;
+    println!("{:?}",  &buf_vec[..]);
+    Ok(())
 }
