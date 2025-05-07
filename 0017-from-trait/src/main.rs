0e253d731af7a2dac2e2bc5b56f47fbe38d9380c
nakayama hiroyuki <yuki@nakayamanoMacBook-Pro.local>
2025-05-07 12:50:18 +0900

    0017 From trait

--- 0017-from-trait/src/main.rs
+++ 0017-from-trait/src/main.rs
@@ -1,3 +1,23 @@
+use std::convert::From;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct NameNumber {
+    name: String,
+    number: i32,
+}
+
+impl From<(String, i32)> for NameNumber {
+    fn from(tuple: (String, i32)) -> Self {
+        NameNumber {
+            name: tuple.0,
+            number: tuple.1,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tuple = ("Alice".to_string(), 42);
+    let name_number: NameNumber = NameNumber::from(tuple);
+    println!("{:?}", name_number);
 }
